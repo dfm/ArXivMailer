19b558ea745dba0fe20f6067e3a0583368678b96
Dan F-M <danfm@nyu.edu>
2016-06-22 20:34:06 +0100

    assets

--- arxivmail/web.py
+++ arxivmail/web.py
@@ -8,9 +8,32 @@ __all__ = ["web"]
 
 web = flask.Blueprint("web", __name__)
 
-@web.route("/")
+@web.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello"
+    if flask.request.method == "POST":
+        email = flask.request.form.get("email", None)
+        if email is None:
+            flask.flash("Missing email address.")
+        category = flask.request.form.get("category", None)
+        if category is None:
+            flask.flash("Missing arXiv category.")
+        if email is not None and category is not None:
+            category = category.strip()
+            email = email.strip()
+            cat = Category.query.filter_by(arxiv_name=category).first()
+            user = Subscriber.query.filter_by(email=email).first()
+            if user is None:
+                user = Subscriber(email)
+            if cat in user.subscriptions:
+                flask.flash("Already subscribed to {0}".format(category))
+            else:
+                user.subscriptions.append(cat)
+                db.session.add(user)
+                db.session.commit()
+                flask.flash("{0} subscribed to {1}".format(email, category))
+
+    categories = Category.query.order_by("arxiv_name").all()
+    return flask.render_template("index.html", categories=categories)
 
 
 @web.route("/manage/<token>")
